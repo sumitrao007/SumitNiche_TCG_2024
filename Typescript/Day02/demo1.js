ab0c2031a6a85d2a29f23fb3d992577b54d68537
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2024-01-05 20:13:15 +0530

    Day02

---
+++ Typescript/Day02/demo1.js
@@ -0,0 +1,28 @@
+// console.log("Welcome to Front End...");
+// console.log('Welcome To My Module By Sumit')
+// console.log(`
+//         Hopes So u r all Enjoying This Session
+// `);
+/*
+
+*/
+var a1;
+var a_1, a$1;
+// data type 
+// number(int,double,long,float,short),string(''/""),boolean(true/false)
+// any (number,string,boolean)
+// void (it is neutral data type => it does not point anything)
+// misc null & undefined => it used value as well as data type 
+var a = 10; // forward declaration & defination
+// console.log("value of a is "+a);
+var str;
+// console.log("Value of str is "+str);
+str = 'Sumit';
+// console.log('Value of str is '+str);
+var b;
+b = 2.5;
+console.log("Value of b is " + b);
+b = 'Sumit';
+console.log("Value of b is " + b);
+b = true;
+console.log("Value of b is " + b);
---
+++ Typescript/Day02/demo1.ts
@@ -0,0 +1,55 @@
+// console.log("Welcome to Front End...");
+// console.log('Welcome To My Module By Sumit')
+// console.log(`
+//         Hopes So u r all Enjoying This Session
+
+// `);
+/*
+
+*/
+
+var a1;
+var a_1,a$1;
+
+// data type 
+// number(int,double,long,float,short),string(''/""),boolean(true/false)
+// any (number,string,boolean)
+// void (it is neutral data type => it does not point anything)
+// misc null & undefined => it used value as well as data type 
+
+
+var a:number=10;   // forward declaration & defination
+// console.log("value of a is "+a);
+
+
+var str!:string;
+// console.log("Value of str is "+str);
+str='Sumit';
+// console.log('Value of str is '+str);
+
+var b:any;
+
+b=2.5;
+// console.log("Value of b is "+b);
+b='Sumit';
+// console.log("Value of b is "+b);
+b=true;
+// console.log("Value of b is "+b);
+
+//Literal
+
+var s:number|string;
+s=9;
+s='sumit';
+
+var s1!:50|string;
+console.log("s1 => "+s1)
+s1='mfbv,dbfvj';
+
+var s2:string|null;
+// s2=null;
+
+var s3:string|undefined;
+// console.log("value of s3 is "+s3);
+
+
