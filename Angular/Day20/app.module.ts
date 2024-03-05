bc68d17b5aaca6f05011926731f6223db493f0aa
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2024-03-05 20:49:11 +0530

    Day20

---
+++ Angular/Day20/app.module.ts
@@ -0,0 +1,95 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { StudentComponent } from './student/student.component';
+import { StringinterpolationComponent } from './stringinterpolation/stringinterpolation.component';
+import { PropertybindingComponent } from './propertybinding/propertybinding.component';
+import { StylebindingComponent } from './stylebinding/stylebinding.component';
+import { ClassBindingComponent } from './class-binding/class-binding.component';
+import { EventbindingComponent } from './eventbinding/eventbinding.component';
+import { Eventbinding2Component } from './eventbinding2/eventbinding2.component';
+import { Eventbinding3Component } from './eventbinding3/eventbinding3.component';
+import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component';
+import { FormsModule } from '@angular/forms';
+import { IfdirectiveComponent } from './ifdirective/ifdirective.component';
+import { FordirectiveComponent } from './fordirective/fordirective.component';
+import { SwitchdirectiveComponent } from './switchdirective/switchdirective.component';
+import { ParentComponent } from './parent/parent.component';
+import { Child1Component } from './child1/child1.component';
+import { Parent1Component } from './parent1/parent1.component';
+import { Child2Component } from './child2/child2.component';
+import { Parent2Component } from './parent2/parent2.component';
+import { Child3Component } from './child3/child3.component';
+import { InbuiltpipeComponent } from './inbuiltpipe/inbuiltpipe.component';
+import { CustompipeComponent } from './custompipe/custompipe.component';
+import { SummaryPipe } from './utility/summary.pipe';
+import { CourseComponent } from './course/course.component';
+import { CourseService } from './utility/course.service';
+import {HttpClientModule} from '@angular/common/http';
+import { PostComponent } from './post/post.component';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { FollowerComponent } from './follower/follower.component';
+import { MyfollwerComponent } from './myfollwer/myfollwer.component';
+import { QueryparameterComponent } from './queryparameter/queryparameter.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ProductComponent } from './product/product.component';
+import { TvComponent } from './tv/tv.component';
+import { LaptopComponent } from './laptop/laptop.component';
+import { MobileComponent } from './mobile/mobile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    StudentComponent,
+    StringinterpolationComponent,
+    PropertybindingComponent,
+    StylebindingComponent,
+    ClassBindingComponent,
+    EventbindingComponent,
+    Eventbinding2Component,
+    Eventbinding3Component,
+    TwowaybindingComponent,
+    IfdirectiveComponent,
+    FordirectiveComponent,
+    SwitchdirectiveComponent,
+    ParentComponent,
+    Child1Component,
+    Parent1Component,
+    Child2Component,
+    Parent2Component,
+    Child3Component,
+    InbuiltpipeComponent,
+    CustompipeComponent,
+    SummaryPipe,
+    CourseComponent,
+    PostComponent,
+    HomeComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    FollowerComponent,
+    MyfollwerComponent,
+    QueryparameterComponent,
+    NotfoundComponent,
+    ProductComponent,
+    TvComponent,
+    LaptopComponent,
+    MobileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [CourseService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/Day20/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  myid:number=9;
+
+  jsonObj={
+    id:9,
+    fname:'Sumit',
+    status:'Active'
+  }
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onPage1(){
+    this.router.navigate(['']);
+  }
+  onPage2(){
+    this.router.navigate(['/Page2',this.myid]);
+  }
+  onPage3(){
+    this.router.navigate(['/Page3']);
+  }
+
+  onFollower(){
+    this.router.navigate(['/Follower']);
+  }
+  onqueryparam(){
+    this.router.navigate(['/Queryparameter'],{
+      queryParams:this.jsonObj
+    });
+  }
+
+}
